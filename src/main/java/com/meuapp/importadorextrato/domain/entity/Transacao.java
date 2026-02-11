ed9e4652e56188717f069b9375cfff9a1a92a04b
thisouza01 <tihsouza01@gmail.com>
2026-02-11 01:32:58 -0300

    Add: Criando classe que representa a tabela no banco de dados.

---
+++ src/main/java/com/meuapp/importadorextrato/domain/entity/Transacao.java
@@ -0,0 +1,104 @@
+package com.meuapp.importadorextrato.domain.entity;
+
+import com.meuapp.importadorextrato.domain.enums.TipoTransacao;
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "tb_transacao")
+public class Transacao {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate data;
+
+    @Column(length = 200, nullable = false)
+    private String descricao;
+
+    @Column(precision = 15, scale = 2)
+    private BigDecimal valor;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private TipoTransacao tipoTransacao;
+
+    @Column(nullable = false)
+    private LocalDateTime dataImportacao;
+
+    @Column(nullable = false)
+    private Long idExecucaoJob;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    public void setValor(BigDecimal valor) {
+        this.valor = valor;
+    }
+
+    public TipoTransacao getTipoTransacao() {
+        return tipoTransacao;
+    }
+
+    public void setTipoTransacao(TipoTransacao tipoTransacao) {
+        this.tipoTransacao = tipoTransacao;
+    }
+
+    public LocalDateTime getDataImportacao() {
+        return dataImportacao;
+    }
+
+    public void setDataImportacao(LocalDateTime dataImportacao) {
+        this.dataImportacao = dataImportacao;
+    }
+
+    public Long getIdExecucaoJob() {
+        return idExecucaoJob;
+    }
+
+    public void setIdExecucaoJob(Long idExecucaoJob) {
+        this.idExecucaoJob = idExecucaoJob;
+    }
+
+    @Override
+    public String toString() {
+        return "Transacao{" +
+                "id=" + id +
+                ", data=" + data +
+                ", descricao='" + descricao + '\'' +
+                ", valor=" + valor +
+                ", tipoTransacao=" + tipoTransacao +
+                ", dataImportacao=" + dataImportacao +
+                ", idExecucaoJob=" + idExecucaoJob +
+                '}';
+    }
+}
