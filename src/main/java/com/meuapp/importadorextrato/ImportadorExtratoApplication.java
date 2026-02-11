6a6638ccd2df92ce6956ffc9ac459a20ef59e69a
thisouza01 <tihsouza01@gmail.com>
2026-02-11 01:19:29 -0300

    Add: Criando setup inicial do projeto.

---
+++ src/main/java/com/meuapp/importadorextrato/ImportadorExtratoApplication.java
@@ -0,0 +1,13 @@
+package com.meuapp.importadorextrato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImportadorExtratoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImportadorExtratoApplication.class, args);
+	}
+
+}
