645eb5f545d6cdc617bd914eafde364cc18e3228
thisouza01 <tihsouza01@gmail.com>
2026-02-21 01:36:42 -0300

    Refact: Adiciona suporte a arquivo dinâmico via JobParameters no Reader.

--- src/main/java/com/meuapp/importadorextrato/batch/reader/ExtratoCSVReader.java
+++ src/main/java/com/meuapp/importadorextrato/batch/reader/ExtratoCSVReader.java
@@ -2,22 +2,28 @@ package com.meuapp.importadorextrato.batch.reader;
 
 import com.meuapp.importadorextrato.domain.dto.TransacaoDTO;
 
+import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 
 @Configuration
 public class ExtratoCSVReader {
 
     @Bean
-    public FlatFileItemReader<TransacaoDTO> reader() {
+    @StepScope
+    public FlatFileItemReader<TransacaoDTO> reader(
+            @Value("#{jobParameters['caminhoArquivo']}") String caminhoArquivo) {
+
 
         return new FlatFileItemReaderBuilder<TransacaoDTO>()
                 .name("LeitorDoExtratoCSV")
-                .resource(new ClassPathResource("extrato-exemplo.csv"))
+                .resource(new FileSystemResource(caminhoArquivo))
                 .linesToSkip(1)
                 .delimited()
                 .delimiter(",")
